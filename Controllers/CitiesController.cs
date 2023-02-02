38694fb6ff86b488983e0fa750696399f43c2690
Preston Lamb <preston.j.lamb@gmail.com>
2023-02-02 11:16:16 -0700

    move models folder; add more documentation output

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -40,6 +40,13 @@ namespace CityInfo.API.Controllers
 			return Ok(_mapper.Map<IEnumerable<CityWithoutPointsOfInterestDto>>(cityEntities));
 		}
 
+		/// <summary>
+		/// Get a city by id
+		/// </summary>
+		/// <param name="id">The id of the city to get</param>
+		/// <param name="includePointsOfInterest">Whether or not to include the points of interest</param>
+		/// <returns>An IActionResult</returns>
+		/// <response code="200">Returns the requested city</response>
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetCity(int id, bool includePointsOfInterest = false)
 		{
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text;
 using CityInfo.API;
 using CityInfo.API.DbContexts;
@@ -5,6 +6,7 @@ using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console().WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day).CreateLogger();
@@ -20,7 +22,31 @@ builder.Services.AddControllers(options =>
 }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+	var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+	setupAction.IncludeXmlComments(xmlCommentsFullPath);
+
+	// setupAction.AddSecurityDefinition("CityInfoApiBearerAuth", new OpenApiSecurityScheme()
+	// {
+	// 	Type = SecuritySchemeType.Http,
+	// 	Scheme = "Bearer",
+	// 	Description = "Input a valid token to access this API"
+	// });
+
+	// setupAction.AddSecurityRequirement(new OpenApiSecurityRequirement
+	// {
+	// 	{
+	// 		new OpenApiSecurityScheme
+	// 		{
+	// 			Reference = new OpenApiReference {
+	// 				Type = ReferenceType.SecurityScheme,
+	// 				Id = "CityInfoApiBearerAuth" }
+	// 		}, new List<string>() }
+	// });
+});
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 builder.Services.AddSingleton<CitiesDataStore>();
 
